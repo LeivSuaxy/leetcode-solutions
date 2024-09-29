d1d431021d1baacd99b9fde354ba967c621435d7
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-09-29 09:29:11 -0700

    Container With Most Water solved.

---
+++ C-Sharp/container-with-most-water.cs
@@ -0,0 +1,29 @@
+﻿public class Solution {
+    public int MaxArea(int[] height)
+    {
+        int medium = height.Length / 2;
+        int max = 0;
+        int pointerLeft = 0;
+        int pointerRigth = height.Length - 1;
+
+        while (pointerLeft != pointerRigth)
+        {
+            int minIndex = Math.Min(height[pointerLeft], height[pointerRigth]);
+            
+            int value = (pointerRigth - pointerLeft) * minIndex;
+
+            if (value > max) max = value;
+
+            if (height[pointerLeft] < height[pointerRigth])
+            {
+                pointerLeft++;
+            }
+            else
+            {
+                pointerRigth--;
+            }
+        }
+
+        return max;
+    }
+}
