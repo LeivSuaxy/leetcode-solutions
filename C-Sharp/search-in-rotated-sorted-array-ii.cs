038cbb715f94b783a5042c900c51126af39a5160
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-09-29 14:11:36 -0700

    Solver Search in rotated sorted array II.

---
+++ C-Sharp/search-in-rotated-sorted-array-ii.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public bool Search(int[] nums, int target)
+    {
+        foreach (int number in nums)
+        {
+            if (number == target) return true;
+        }
+
+        return false;
+    }
+}
