cad58dc2763bc3742e6bb69a5f398e5e54d460a9
LeivSuaxy <127800936+LeivSuaxy@users.noreply.github.com>
2024-10-07 06:51:23 -0700

    Added daily 7/10/2024

---
+++ C-Sharp/minimum-string-length-after-removing-substrings.cs
@@ -0,0 +1,17 @@
+﻿namespace DefaultNamespace;
+
+public class Solution
+{
+    public int MinLength(string s) {
+        if (s.Length == 0) return 0;
+
+        string cadena1 = "AB", cadena2 = "CD";
+
+        while (s.Contains(cadena1) || s.Contains(cadena2))
+        {
+            s = s.Replace(cadena1, "").Replace(cadena2, "");
+        }
+
+        return s.Length;
+    }
+}
