d49f99e2b3fd2b5f5ed384d6c2938d8c95c8f0e4
jleivsuaxy <jleivsuaxy@gmail.com>
2025-01-08 19:07:06 -0500

    Two Sum js.

--- JS/1.js
+++ JS/1.js
@@ -5,7 +5,7 @@ var twoSum = function(nums, target) {
 
     for(let i = 0; i < nums.length - 1; i++){
         for(let j = i + 1; j < nums.length; j++ ){
-            if ( target - nums[j] == nums[i]){
+            if (target - nums[j] === nums[i]){
                 arrayNums[0] = i;
                 arrayNums[1] = j;
                 return arrayNums;
